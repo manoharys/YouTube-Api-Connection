3c19967413e45a172ecc48dc4d29047a546a711e
manohar <manumanohar1527@gmail.com>
2020-05-02 13:45:40 +0530

    handling response

--- app.js
+++ app.js
@@ -1,11 +1,37 @@
 const API = "AIzaSyCf9kMuYtYfP-o4k6haBVRTLFCV74bnqlQ";
 const output = document.querySelector('.output');
-function ySearch(e){
-    const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key='+API+'&q=test&maxResults=20';
-   output.textContent = url;
-    fetch(url).then(function(rep){
+const searchTerm = document.querySelector('input');
+const btn = document.querySelector('button');
+
+searchTerm.setAttribute('value', "test");
+btn.addEventListener('click', ySearch);
+
+function ySearch() {
+    let search = encodeURIComponent(searchTerm.value);
+    const url = 'https://www.googleapis.com/youtube/v3/search/?part=snippet&key=' + API + '&q=' + search + 't&maxResults=20';
+    output.textContent = url;
+    fetch(url).then(function (rep) {
         return rep.json()
-    }).then(function(data){
-        console.log(data);
+    }).then(function (data) {
+        console.log( data);
+        showData(data.items);
+    })
+}
+
+
+
+function showData(data) {
+    console.log(data);
+    console.log(data.length);
+    data.forEach(function (video) {
+        console.log(video);
+        let div = document.createElement('div');
+        div.classList.add('box');
+        let temp = document.createTextNode(video.snippet.description);
+        div.appendChild(temp);
+        let span = document.createElement('span');
+        span.innerHTML = '<a href="http://www.youtube.com/watch?v=' + video.id.videoId + '" target="_blank">' + video.snippet.title + '</a>';
+        div.appendChild(span);
+        output.appendChild(div);
     })
 }
